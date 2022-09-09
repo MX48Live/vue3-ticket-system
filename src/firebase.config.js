1f88c4b790c5175337a55f83a9e9af7faca014bd
MX48@Live <MX48Live@Gmail.com>
2022-09-09 15:51:02 +0700

    Fix env

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,11 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID
+// }
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  authDomain: "db-vue3-ticket-system.firebaseapp.com",
+  projectId: "db-vue3-ticket-system",
+  storageBucket: "db-vue3-ticket-system.appspot.com",
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID
 }
