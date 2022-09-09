f8a852e05cc390eccd84ff3ad29feba41bc9933c
MX48@Live <MX48Live@Gmail.com>
2022-09-09 16:25:54 +0700

    Fix poppersjs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import '@popperjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
